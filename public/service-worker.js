33a42ee3bdfbec998d416af0cdb262ea7119de24
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-05-17 21:55:39 -0500

    storage

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('static').then( cache => {
+        return cache.addAll([
+          './',
+          './index.html',
+          './icons/icon-192x192.png',
+          './icons/icon-512x512.png',
+          './index.js'
+        ]);
+      })
+    );
+    console.log('Install');
+    self.skipWaiting();
+  });
+  
+  // retrieve assets from cache
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then( response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
